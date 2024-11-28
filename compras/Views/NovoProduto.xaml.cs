e88bb41864448701a67babb2561526df2124b90c
Usuario <Usuario@CPD03-M06>
2024-11-28 16:01:31 -0300

    Adicionar arquivos de projeto.

---
+++ compras/Views/NovoProduto.xaml.cs
@@ -0,0 +1,32 @@
+using compras.Models;
+
+namespace compras.Views;
+
+public partial class NovoProduto : ContentPage
+{
+	public NovoProduto()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e) 
+	{
+		try
+		{
+			Produto p = new Produto()
+			
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text),
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro inserido", "OK");
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+	}
+}
